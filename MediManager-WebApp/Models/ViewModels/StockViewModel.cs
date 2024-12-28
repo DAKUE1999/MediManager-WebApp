c0b55940fd891827fe71479cc07f6beda394c5a3
DAKUE1999 <86852740+DAKUE1999@users.noreply.github.com>
2024-12-28 21:04:09 +0100

    Add StockViewModel and MedicationSelectionViewModel

---
+++ MediManager-WebApp/Models/ViewModels/StockViewModel.cs
@@ -0,0 +1,29 @@
+namespace MediManager_WebApp.Models.ViewModels
+{
+    public class StockViewModel
+    {
+        public int ID { get; set; }
+        public int WarehouseID { get; set; }
+        public int MedicationGroupID { get; set; }
+        public int MedicationID { get; set; }
+        public int ShelfID { get; set; }
+        public string Batch { get; set; }
+        public string SerialNumber { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public int Quantity { get; set; }
+
+        // Zusätzliche Properties für die UI
+        public string WarehouseName { get; set; }
+        public string MedicationGroupName { get; set; }
+        public string MedicationName { get; set; }
+        public string ShelfName { get; set; }
+    }
+
+    public class MedicationSelectionViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string PZN { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
