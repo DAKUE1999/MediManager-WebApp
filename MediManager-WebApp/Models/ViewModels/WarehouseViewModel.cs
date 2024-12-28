32eae2d96b044b30178a1addf7f858b5373d68f8
DAKUE1999 <86852740+DAKUE1999@users.noreply.github.com>
2024-12-28 14:44:36 +0100

    Add WarehouseViewModel with Shelves support

---
+++ MediManager-WebApp/Models/ViewModels/WarehouseViewModel.cs
@@ -0,0 +1,18 @@
+namespace MediManager_WebApp.Models.ViewModels
+{
+    public class WarehouseViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public List<ShelfViewModel> Shelves { get; set; } = new List<ShelfViewModel>();
+    }
+
+    public class ShelfViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
